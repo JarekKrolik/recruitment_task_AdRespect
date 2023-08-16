410c825b38d7674d46263802edb67ce6d17f9515
JarekKrolik <madmax@onet.eu>
2023-08-16 21:44:27 +0200

    added animations in dropdown menu

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const magnifyingGlass = document.querySelector(".magnifying-glass");
 const searchForm = document.querySelector("form.d-flex");
+const dropDownMenuElements = document.getElementsByClassName("dropdown-item");
+let animationDelay = 0;
 magnifyingGlass.addEventListener("click", () => {
   searchForm.classList.toggle("on");
 });
+
+const dropDownMenuDelayHandler = () => {
+  animationDelay = 0;
+  for (let i = 0; i < dropDownMenuElements.length; i++) {
+    animationDelay += 0.2;
+    dropDownMenuElements[i].setAttribute(
+      "style",
+      `animation-delay:${animationDelay}s`
+    );
+  }
+};
+dropDownMenuDelayHandler();
