f9ce61af1c2474809beebd50584eabf250949850
JarekKrolik <madmax@onet.eu>
2023-08-22 00:22:31 +0200

    added masonry style gallery

--- script.js
+++ script.js
@@ -4,12 +4,46 @@ const dropDownMenuElements = document.getElementsByClassName("dropdown-item");
 const heroButtons = document.querySelectorAll(".hero-block__button");
 const heroText = document.querySelector(".hero-block__text");
 const heroPicture = document.querySelector(".hero-block__picture");
+const fogOffButton = document.querySelector(".projects__fog-button");
+const fogOnButton = document.querySelector(".projects-button");
+const fogItems = document.querySelectorAll(".fog");
+const projectsGallery = document.querySelector(".projects__pictures");
+const projectAllPictures = document.querySelectorAll(".projects__picture");
+const carousel = document.querySelector(".carousel");
+const carouselOffBtn = document.querySelector(".carousel__exit-btn");
 // let heroSliderTimer = setInterval(heroSlider, 5000);
 let animationDelay = 0;
 AOS.init();
 magnifyingGlass.addEventListener("click", () => {
   searchForm.classList.toggle("on");
 });
+
+const macyInstance = Macy({
+  container: ".projects__pictures",
+  mobileFirst: "true",
+  columns: 1,
+  breakAt: {
+    400: 2,
+    1200: 3,
+  },
+  margin: {
+    x: 30,
+    y: 30,
+  },
+});
+
+const fogOffHandler = () => {
+  fogItems.forEach((e) => {
+    e.classList.toggle("fog-off");
+  });
+};
+const projectsPicturesPopUpHandler = () => {
+  fogOffHandler();
+  projectsGallery.classList.toggle("pictures-off");
+  carousel.classList.toggle("pictures-off");
+  fogOnButton.classList.toggle("fog-off");
+};
+
 function heroSlider() {
   // clearInterval(heroSliderTimer);
   heroText.classList.toggle("left");
@@ -30,7 +64,12 @@ const dropDownMenuDelayHandler = () => {
 };
 
 dropDownMenuDelayHandler();
-
+projectAllPictures.forEach((e) => {
+  e.addEventListener("click", projectsPicturesPopUpHandler);
+});
+fogOffButton.addEventListener("click", fogOffHandler);
+fogOnButton.addEventListener("click", fogOffHandler);
+carouselOffBtn.addEventListener("click", projectsPicturesPopUpHandler);
 heroButtons.forEach((e) => {
   e.addEventListener("click", heroSlider);
 });
